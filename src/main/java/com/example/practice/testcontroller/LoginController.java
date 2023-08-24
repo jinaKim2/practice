7251a29e3cf094916407474ec006d096ae37a7de
khs <khs@fresheasy.com>
2023-08-24 20:26:07 +0900

    김한상 샘플파일 추가

---
+++ src/main/java/com/example/practice/testcontroller/LoginController.java
@@ -0,0 +1,88 @@
+package com.example.practice.testcontroller;
+
+import com.example.practice.persistance.UserVO;
+import com.example.practice.testservice.TestService;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+@RequestMapping("")
+public class LoginController {
+
+    /**
+     * 로그인 구현 컨트롤러
+     * */
+
+    @Autowired
+    TestService testService;
+
+    @GetMapping("/main.do")
+    public ModelAndView main(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("msg", "서버에서 받아온 값 입니다.");
+        // modelAndView.setViewName("page/main/index");
+        modelAndView.setViewName("page/main/index2");
+        return modelAndView;
+    }
+
+    @PostMapping("/find.do")
+    public UserVO find(@RequestBody UserVO userVo){
+
+        String id = userVo.getUserId();
+        String pw = userVo.getUserPw();
+
+/*
+        String name = "";
+
+        if(id.equals("khs88")) {
+            name = "김한상";
+        } else if(id.equals("kja01")) {
+            name = "김진아";
+        } else {
+            name = "알 수 없음";
+        }
+
+        UserVO findResult = new UserVO();
+        findResult.setUserId(id);
+        findResult.setUserPw(pw);
+        findResult.setUserNm(name);
+*/
+
+        UserVO findResult = testService.find(id, pw);
+
+        return findResult;
+        /*
+         * 5. 비고
+         *  1). "kjaS2khs" 데이터 삭제 영구 금ㅈ;
+         * */
+    }
+
+    @PostMapping("/insert.do")
+    public void insert(HttpServletRequest req){
+
+        String id = req.getParameter("id");
+        String pw = req.getParameter("pw");
+
+        testService.insert(id, pw);
+    }
+
+    @PutMapping("/update.do")
+    public void update(HttpServletRequest req){
+
+        String id = req.getParameter("id");
+        String pw = req.getParameter("pw");
+
+        testService.update(id, pw);
+    }
+
+    @DeleteMapping("/delete.do")
+    public void delete(HttpServletRequest rep){
+
+        String id = rep.getParameter("id");
+
+        testService.delete(id);
+    }
+
+}
