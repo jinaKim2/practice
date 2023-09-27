718d4f92f7d2e7fc29afc3a0e1758efbd1118f4a
jina <kja7698@naver.com>
2023-09-27 12:17:20 +0900

    ㅋㅓ밋 테스트

--- src/main/java/com/example/practice/jinaBoard/boardcontroller/BoardController.java
+++ src/main/java/com/example/practice/jinaBoard/boardcontroller/BoardController.java
@@ -17,7 +17,7 @@ public class BoardController {
 
     @Autowired
     BoardService boardService;
-
+    /* git test */
     @GetMapping("/main.do")
     public ModelAndView main(){
         ModelAndView modelAndView = new ModelAndView();
@@ -37,8 +37,16 @@ public class BoardController {
     }
     @PostMapping("/registBoard.do")
     public int insert(@RequestBody BoardVO boardVO){
+        // 1. 첫번째 테이블 인서트
+        boardService.insertTb1(boardVO);
 
-        int result = boardService.insert(boardVO);
+       // 2. 첫번째 테이블인서트 된 키(id) 를 가져오기 위한 조회
+       BoardVO selectKey = boardService.findRefId(boardVO);
+       // 3. 두번째 테이블의 ref_id에 값을 넣어주기 위한 세팅
+        boardVO.setRefId(selectKey.getId());
+
+       // 두번째 insert시 result는 화면에 결과값 건네주기 위한 용도, 있어야 함
+        int result = boardService.insertTb2(boardVO)
 
         return result;
     }
